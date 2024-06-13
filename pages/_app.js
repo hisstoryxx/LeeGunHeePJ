91813c7ca5fbb06f59e6f15622a20bb647fc33b3
cho <hisstoryxx@gmail.com>
2024-06-13 22:52:39 +0900

    feat:login 창 생성중

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,32 @@ import '@fortawesome/fontawesome-svg-core/styles.css'
 config.autoAddCss = false
 import Footer from '@/src/component/Footer/Footer'
 import Top from '@/src/component/Top/Top'
-import '../styles/style.css'
+import styles from  './App.module.css'
 import Login from './Login/Login'
 
 
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps, }) {
   return (
     // <div style = {{ width : 1000, margin : "0 auto"}}>
-    <div>
+    <div style ={{width: '100%', height: '100%'}}>
+      
+      <div className={styles.mainPage}>
+      <div className={styles.sectionImg}>
+        <div className={styles.infoDiv}>
+          <div className={styles.mainLogo} alt="main illustration"/>
+          <h1>이건희과제 WebDb Site</h1>
+        </div>
+      </div>
+      <div className={styles.sectionContent}>
       <Login/>
+        {/* {
+          returnComponent(prop?.page)
+        } */}
+      </div>
+    </div>
+
+      
       {/* <Top/>
       <Component {...pageProps} />
       <Footer/> */}
